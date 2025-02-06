a8160392256b5fc55f996b36589e7bc706aa433a
Alysson <alysson.gabriel61@gmail.com>
2025-02-06 12:38:26 -0400

    change: altera cor do header

--- includes/header.php
+++ includes/header.php
@@ -6,11 +6,11 @@
     <title>Vendaplus</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body class="bg-dark text-light">
+  <body class="bg-dark">
 
     <div class="container">
 
-        <div class="mt-3 bg-danger p-4">
+        <div class="mt-3 p-4 text-light bg-gradient" style="background-color: #023e8a;">
             <h1>Vendaplus</h1>
             <p>Sistema para gestão de vendas</p>
         </div>
