162dc9d709fe0a7db000cac9aea3a75d7b3ceb22
Alysson <alysson.gabriel61@gmail.com>
2025-02-16 19:00:33 -0400

    fix: ajusta a responsividade do form

--- app/includes/formulario.php
+++ app/includes/formulario.php
@@ -9,54 +9,50 @@
         <h2 class="mt-3">Registrar venda</h2>
 
         <!-- Container do formulário -->
-        <div class="form-container pb-4 w-50">
+        <div class="form-container pb-4 w-50 w-sm-75 w-md-50 w-lg-35 w-xl-25">
            
-                <form id="form-venda" method="post">    
+            <form id="form-venda" method="post">    
 
-                    <!-- input produtos -->
-                    <div class="form-group mt-3">
-                        <label for="produtos">Produtos:</label>
-                        <textarea class="form-control" name="produtos" id="produtos"></textarea>
-                    </div>
+                <!-- input produtos -->
+                <div class="form-group mt-3">
+                    <label for="produtos">Produtos:</label>
+                    <textarea class="form-control" name="produtos" id="produtos" placeholder="Egeo, Hidratante Deleite, etc." required></textarea>
+                </div>
 
-                    <!-- input valor -->
-                    <div class="form-group mt-3">
-                        <label for="valor">Valor:</label>
-                        <input class="form-control" type="text" name="valor" id="valor">
-                    </div>
+                <!-- input valor -->
+                <div class="form-group mt-3">
+                    <label for="valor">Valor:</label>
+                    <input class="form-control" type="text" name="valor" id="valor" placeholder="R$" required>
+                </div>
 
-                    <!-- input cliente -->
-                    <div class="form-group mt-3">
-                        <label for="cliente">Cliente:</label>
-                        <input class="form-control" type="text" name="cliente" id="cliente">
-                    </div>
+                <!-- input cliente -->
+                <div class="form-group mt-3">
+                    <label for="cliente">Cliente:</label>
+                    <input class="form-control" type="text" name="cliente" id="cliente" required>
+                </div>
 
-                    <!-- input status -->
-                    <div class="form-group mt-3">
-                        <label>Status da venda</label>
-
-                        <div>
-                            <div class="px-0 form-check form-check-inline">
-                                <label class="form-control">
-                                    <input type="radio" name="status" id="status" value="S"> Pago
-                                </label>
-                            </div>
-                            
-                            <div class="form-check form-check-inline">
-                                <label class="form-control">
-                                    <input type="radio" name="status" id="status" value="N" checked> Pendente
-                                </label>
-                            </div>
+                <!-- input status -->
+                <div class="form-group mt-3">
+                    <label>Status da venda</label>
+                    <div class="d-flex flex-column flex-sm-row align-items-center justify-content-start">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="status" id="statusPago" value="S">
+                            <label class="form-check-label" for="statusPago">Pago</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="status" id="statusPendente" value="N" checked>
+                            <label class="form-check-label" for="statusPendente">Pendente</label>
                         </div>
                     </div>
-                            
-                    <!-- registrar a venda -->
-                    <div class="form-group mt-3">
-                        <button id="registrar" type="button" class="btn btn-success">Registrar</button>
-                    </div>
+                </div>
+                        
+                <!-- registrar a venda -->
+                <div class="form-group mt-3">
+                    <button id="registrar" type="button" class="btn btn-success">Registrar</button>
+                </div>
 
-                </form>
-            </div>
-       
+            </form>
+        </div>
+   
     </div>
 </main>
