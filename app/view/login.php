8eba711d99e5d0b7ca6c1ecce4a10369b4f285fb
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-03-16 07:57:01 -0400

    refactor: aplica configs de estilo para centralizar o login

--- app/view/login.php
+++ app/view/login.php
@@ -7,14 +7,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
 
+        html, body{
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+        }
+
         .alert{
             display: none;
         }
 
         .container{
             width: 60%;
-            margin: 0 auto; /* Adiciona margem automática horizontal */
-            margin-top: 100px; /* Adiciona margem no topo para centralizar verticalmente (ajuste conforme necessário) */
+            margin: auto; /* Adiciona margem no topo para centralizar verticalmente (ajuste conforme necessário) */
         }
 
         #img-box{
