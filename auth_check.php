2f6a92c3a7d7eca16ca85e88784b54264d7917e1
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2025-08-02 21:55:40 -0400

    edit auth check

--- auth_check.php
+++ auth_check.php
@@ -2,9 +2,8 @@
 
 session_start();
 
-// Check if the user is logged in (adjust the session key as needed)
 if (!isset($_SESSION['id_user'])) {
-    // If not logged in, redirect to the login page
+
     header("Location: login"); 
     exit(); 
 }
